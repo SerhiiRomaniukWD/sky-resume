a2c2806e5d1007af1a5c5f1eb40b0891103b33dc
serhii.dev <serhii.dev@serhiidevs-MacBook-Pro.local>
2025-07-02 11:38:07 +0200

    added base layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export const App: React.FC = () => {
   return (
-    <>
-      <h1>Serhii's resume</h1>
-    </>
+    <div
+     className="flex items-center justify-center px-20 h-screen bg-[#b8d7f2]"
+    >
+      <div className="h-[80vh] w-full bg-white rounded-lg">
+        <h1>Serhii's resume</h1>
+      </div>
+    </div>
   );
 };
