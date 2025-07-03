1763b9cf0c36164d2ed2d02e702155e1d0254ac9
serhii.dev <serhii.dev@serhiidevs-MacBook-Pro.local>
2025-07-03 17:53:58 +0200

    created main layout

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import clsx from "clsx";
 
 export const Skills: React.FC = () => {
-  return <section className="bg-primary-blue-light/50" id="skills"></section>;
+  const [isScrolledTo, setIsScrolledTo] = useState(false);
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const sectionEl = sectionRef.current;
+    if (!sectionEl) return;
+
+    const observer = new window.IntersectionObserver(
+      ([entry]) => {
+        setIsScrolledTo(entry.isIntersecting && entry.intersectionRatio > 0.05);
+      },
+      {
+        root: null,
+        threshold: 0.5,
+      },
+    );
+
+    observer.observe(sectionEl);
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <section className="!px-0" id="skills">
+      <div
+        ref={sectionRef}
+        className={clsx(
+          "area transition-all duration-500",
+          isScrolledTo ? "rounded-none" : "rounded-[20rem]",
+        )}
+      >
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+
+      <div className="px-20 py-40">
+        <h2 className="font-bungee text-4xl">
+          I am a problem <span className="text-white">solver</span>
+        </h2>
+      </div>
+    </section>
+  );
 };
