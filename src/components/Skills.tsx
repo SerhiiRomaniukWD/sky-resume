3990d4eeeb3165f8ea4a56635853d004ec85a27c
serhii.dev <serhii.dev@serhiidevs-MacBook-Pro.local>
2025-07-04 10:35:37 +0200

    added list of skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
 import clsx from "clsx";
 
+import JSIcon from "../assets/icons/js.svg";
+import TSIcon from "../assets/icons/ts.svg";
+import ViteIcon from "../assets/icons/vite.svg";
+import GitIcon from "../assets/icons/git.svg";
+import ReduxIcon from "../assets/icons/redux.svg";
+import ZustandIcon from "../assets/icons/zustand.svg";
+import TailwindIcon from "../assets/icons/tailwind.svg";
+import ReactIcon from "../assets/icons/react.svg";
+
 export const Skills: React.FC = () => {
   const [isScrolledTo, setIsScrolledTo] = useState(false);
   const sectionRef = useRef<HTMLDivElement>(null);
@@ -25,7 +34,7 @@ export const Skills: React.FC = () => {
   }, []);
 
   return (
-    <section className="!px-0" id="skills">
+    <section className="flex flex-col justify-between !px-0 pb-40" id="skills">
       <div
         ref={sectionRef}
         className={clsx(
@@ -47,10 +56,75 @@ export const Skills: React.FC = () => {
         </ul>
       </div>
 
-      <div className="px-20 py-40">
-        <h2 className="font-bungee text-4xl">
+      <div className="px-20 pt-40 overflow-hidden">
+        <h2
+          className={clsx(
+            "font-bungee text-4xl mb-20 translate-x-[calc(50%-15rem)] duration-500",
+            isScrolledTo && "!translate-x-0",
+          )}
+        >
           I am a problem <span className="text-white">solver</span>
         </h2>
+
+        <div
+          className={clsx(
+            "flex flex-col translate-x-[calc(50%-19rem)] duration-500",
+            isScrolledTo && "!translate-x-[calc(100%-36rem)]",
+          )}
+        >
+          <p
+            className={clsx(
+              "w-[38rem] mb-8 font-montserrat text-primary-black/60 text-center pr-0",
+              isScrolledTo && "text-right pr-20",
+            )}
+          >
+            I'm Serhii, a Software Developer working remotly in sunny Italy.
+          </p>
+
+          <p
+            className={clsx(
+              "w-[38rem] font-montserrat text-primary-black/60 text-center pr-0",
+              isScrolledTo && "text-right pr-20",
+            )}
+          >
+            Over the past 2 years, I've worked in the web industry, in
+            particular, I have been involved in front-end development and a
+            little bit of back-end. I am satisfied with my experience and
+            continue to work on getting better.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <ul className="flex flex-col items-center gap-4 px-20 font-montserrat text-2xl text-primary-black">
+          <li className="flex items-center gap-2">
+            <JSIcon /> JavaScript
+          </li>
+          <li className="flex items-center gap-2">
+            <TSIcon /> TypeScript
+          </li>
+          <li className="flex items-center gap-2">
+            <ReactIcon /> React
+          </li>
+          <li className="flex items-center gap-2">
+            <TailwindIcon /> Tailwindcss
+          </li>
+        </ul>
+
+        <ul className="flex flex-col items-center gap-4 px-20 font-montserrat text-2xl text-primary-black">
+          <li className="flex items-center gap-2">
+            <ReduxIcon /> Redux
+          </li>
+          <li className="flex items-center gap-2">
+            <ZustandIcon /> Zustand
+          </li>
+          <li className="flex items-center gap-2">
+            <GitIcon /> Git
+          </li>
+          <li className="flex items-center gap-2">
+            <ViteIcon /> Vite
+          </li>
+        </ul>
       </div>
     </section>
   );
