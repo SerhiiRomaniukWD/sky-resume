08a31ba9e1a4924e7790ff8e9156268789ca5127
serhii.dev <serhii.dev@serhiidevs-MacBook-Pro.local>
2025-07-16 11:33:55 +0200

    removed logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 
-import { Logo } from "./Logo";
 import { Nav } from "./Nav";
 
 export const Header: React.FC = () => {
   return (
     <header className="hidden lg:block">
-      <Logo />
-
       <Nav />
     </header>
   );
